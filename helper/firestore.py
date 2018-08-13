23fd38c80828d45457308e9ab4f5dae1efc11168
Thada Wangthammang <mildronize@gmail.com>
2018-08-14 03:40:09 +0700

    fix new line in string of privatekey

--- helper/firestore.py
+++ helper/firestore.py
@@ -10,7 +10,7 @@ def init_env():
         "type": "service_account",
         "project_id": "technukrom",
         "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
-        "private_key": os.environ.get('PRIVATE_KEY'),
+        "private_key": os.environ.get('PRIVATE_KEY').replace('\\n','\n'),
         "client_email": "data-crawler@technukrom.iam.gserviceaccount.com",
         "client_id": "110400317028019650866",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -18,7 +18,7 @@ def init_env():
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/data-crawler%40technukrom.iam.gserviceaccount.com"
     }
-    print(service_key)
+
     cred = credentials.Certificate(service_key)
     firebase_admin.initialize_app(cred)
     return firestore.client()
