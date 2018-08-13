2710ce8b6c990d74e8ccdd2e7a8658b3a3eefb40
Thada Wangthammang <mildronize@gmail.com>
2018-08-14 02:32:54 +0700

    add feed to firestore, fix for building with travis

--- data_crawler.py
+++ data_crawler.py
@@ -5,23 +5,68 @@
 import xmltodict
 # import requests
 import feedparser
+import hashlib
 import json
-from pprint import pprint
+from time import mktime
+from datetime import datetime
 
-def data_crawler():
+from pprint import pprint
+from helper import firestore
+
+ENCODING = "utf-8"
+
+def hash_id(feed):
+    m = hashlib.sha1()
+    m.update(feed['id'].encode())
+    m.update(feed['rss_link'].encode())
+    return m.hexdigest()
+
+def feed_extractor():
+    result = []
+
     with open('data/data-sources.json') as f:
         data_sources = json.load(f)
 
     for key, sources in data_sources.items():
         for source in sources:
-            print(source)
             d = feedparser.parse(source)
-            print(d.feed['title'])
-            print(d.feed['link'])
-            print('-'*30)
             for entry in d.entries:
-                pprint(entry)
-                print('#'*10)
+                result.append({})
+                current_item = len(result)-1
+                result[current_item]['site_name'] = d.feed['title']
+                result[current_item]['rss_link'] = d.feed['link']
+
+                result[current_item]['title'] = entry['title']
+                result[current_item]['author'] = entry['author']
+                result[current_item]['content'] = entry['content'][0]['value']
+                result[current_item]['published'] = datetime.fromtimestamp(mktime(entry['published_parsed']))
+                result[current_item]['updated'] = datetime.fromtimestamp(mktime(entry['updated_parsed']))
+                result[current_item]['id'] = entry['id']
+                result[current_item]['link'] = entry['link']
+                result[current_item]['tags'] = []
+                for tag in entry['tags']:
+                    result[current_item]['tags'].append(tag['term'])
+                result[current_item]['hash_id'] = hash_id(result[current_item])
+    return result
+
+def data_crawler():
+    db = firestore.init_env()
+    collection_name = u'posts'
+    doc_ref = db.collection(collection_name)
+
+    # get all documents
+    docs = db.collection(collection_name).get()
+    available_feeds = []
+    for doc in docs:
+        available_feeds.append(doc.id)
+
+    feeds = feed_extractor()
+    for feed in feeds:
+        if feed['hash_id'] not in available_feeds:
+            doc_ref.document(feed['hash_id']).set(feed)
+            print("DONE: "+ feed['title'])
+        else:
+            print("SKIP: "+ feed['title'])
 
 data_crawler()
-#
+
