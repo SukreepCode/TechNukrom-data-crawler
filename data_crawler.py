5a777788416a64fd5bbba2f1194047365ecf4615
Thada Wangthammang <mildronize@gmail.com>
2018-08-16 13:50:29 +0700

    fix on build error, if no author field, add domain instead. check if no `tags` field, skip it!

--- data_crawler.py
+++ data_crawler.py
@@ -9,6 +9,7 @@ import hashlib
 import json
 from time import mktime
 from datetime import datetime
+from urllib.parse import urlparse
 
 from pprint import pprint
 from helper import firestore
@@ -21,6 +22,19 @@ def hash_id(feed):
     m.update(feed['rss_link'].encode())
     return m.hexdigest()
 
+def url_to_base_url(url):
+    # Input:  https://dev.mildronize.com/hello/world
+    # Return: https://dev.mildronize.com/
+    return '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(url))
+
+def url_to_domain(url):
+    # Input:  https://dev.mildronize.com/hello/world
+    # Return: dev.mildronize.com
+    return '{uri.netloc}'.format(uri=urlparse(url))
+
+# gives
+'http://stackoverflow.com/'
+
 def feed_extractor():
     result = []
 
@@ -29,24 +43,34 @@ def feed_extractor():
 
     for key, sources in data_sources.items():
         for source in sources:
+            print("Source: {}".format(source))
+            domain = url_to_domain(source)
+
             d = feedparser.parse(source)
             for entry in d.entries:
                 result.append({})
                 current_item = len(result)-1
                 result[current_item]['site_name'] = d.feed['title']
                 result[current_item]['rss_link'] = d.feed['link']
-
+                pprint(entry)
                 result[current_item]['title'] = entry['title']
-                result[current_item]['author'] = entry['author']
+                if 'author' in entry:
+                    result[current_item]['author'] = entry['author']
+                else:
+                    result[current_item]['author'] = domain
                 result[current_item]['content'] = entry['content'][0]['value']
                 result[current_item]['published'] = datetime.fromtimestamp(mktime(entry['published_parsed']))
-                result[current_item]['updated'] = datetime.fromtimestamp(mktime(entry['updated_parsed']))
+                if 'updated_parsed' in entry:
+                    result[current_item]['updated'] = datetime.fromtimestamp(mktime(entry['updated_parsed']))
+                else:
+                    result[current_item]['updated'] = result[current_item]['published']
                 result[current_item]['created'] = datetime.now()
                 result[current_item]['id'] = entry['id']
                 result[current_item]['link'] = entry['link']
-                result[current_item]['tags'] = []
-                for tag in entry['tags']:
-                    result[current_item]['tags'].append(tag['term'])
+                if 'tags' in entry:
+                    result[current_item]['tags'] = []
+                    for tag in entry['tags']:
+                        result[current_item]['tags'].append(tag['term'])
                 result[current_item]['hash_id'] = hash_id(result[current_item])
     return result
 
